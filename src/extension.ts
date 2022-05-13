26a76668d0e5d816cd81f05ab6089a8593b5fd8f
Vlad Minaev <minaev@haulmont.com>
2022-05-13 21:02:25 +0200

    Add 3rd party module and info provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,23 @@
 import * as vscode from 'vscode';
-import { DenoModulesProvider } from './DenoModulesProvider';
+import { DenoInfoViewProvider } from './InfoViewProvider';
+import { DenoModulesTreeProvider } from './ModulesTreeProvider';
 
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
-	? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+		? vscode.workspace.workspaceFolders[0].uri.fsPath 
+		: undefined;
 
-	const disposable = vscode.commands.registerCommand('deno-modules.helloWorld', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('deno-modules.helloWorld', () => {
 		//
-	});
+	}));
 
-	const nodeDependenciesProvider = new DenoModulesProvider(rootPath);
-	vscode.window.registerTreeDataProvider('denoModules', nodeDependenciesProvider);
+	const modulesTreeProvider = new DenoModulesTreeProvider(rootPath);
+	context.subscriptions.push(vscode.window.registerTreeDataProvider('denoModules', modulesTreeProvider));
 
-	context.subscriptions.push(disposable);
+	const infoViewProvider = new DenoInfoViewProvider(context.extensionUri)
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider('denoInfo', infoViewProvider));
 }
 
 // this method is called when your extension is deactivated
